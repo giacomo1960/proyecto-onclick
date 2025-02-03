4f21c568b6bdc4fe26bf6e99981416c31182b295
Giacomo <giacomo.villaureta@gmail.com>
2025-02-03 15:45:25 -0300

    abriendo solucion curso 2 modulo 104

---
+++ app alertaalertFunction.js
@@ -0,0 +1,10 @@
+   let titulo =document.querySelector('h1');
+   titulo.innerHTML="Juego del número secreto" 
+
+   let parrafo = document.querySelector('p');
+    parrafo.innerHTML="Selecciona un número de 1 a 10";
+
+
+    function alertFunction() {
+        alert("¡Hola, has hecho clic en el botón!");
+      }
